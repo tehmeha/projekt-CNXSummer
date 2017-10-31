30d1bab9b0c45cd431491c9da90c0cfd45f1bb32
CNXSummer <lukamata99@gmail.com>
2017-10-31 15:33:51 +0100

    Program

--- program.cpp
+++ program.cpp
@@ -3,5 +3,30 @@ using namespace std;
 
 int main()
 {
+    bool sit=true;
+    int stupac,red;
+    int polje2d[9][9]={{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
+   do{
+        cout <<"Unesite stupac u koji zelite unjeti krizic/kruzic:";
+        cin >>stupac;
+        cout <<"Unesite red u koji zelite unjeti krizic/kruzic:";
+        cin >>red;
+        if(stupac <=8 && stupac >0 && red <=8  && red > 0)
+        {
+          cin >> polje2d[stupac][red];
+          for(int i=0;i<8;i++)
+          {
+              for(int j=0;j<8;j++)
+              {
+                cout << polje2d[i][j]<<"\t";
+              }
+              cout <<endl;
+          }
+        }
+        else
+        {
+            cout <<"Unjeli ste krive podatke."<<endl;
+        }
+    }while(sit=true);
 
 }
