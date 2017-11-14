034eb8eca86264cb1027ab0de921b60fe19cfc8d
CNXSummer <lukamata99@gmail.com>
2017-11-14 14:32:39 +0100

    .

--- program.cpp
+++ program.cpp
@@ -13,11 +13,16 @@ int main()
         cin >>red;
         if(stupac <=8 && stupac >0 && red <=8  && red > 0)
         {
-          cin >> polje2d[stupac][red];
+          cin >> polje2d[stupac-1][red-1];
           for(int i=0;i<8;i++)
           {
+
               for(int j=0;j<8;j++)
               {
+                if(j==3 || j==6)
+                {
+                    cout<<"\t";
+                }
                 cout << polje2d[i][j]<<"\t";
               }
               cout <<endl;
